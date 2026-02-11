3e0a4af57965ba802d90baf4bc62e18568f9222f
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-11 13:27:35 -0400

    feat(app): user entity added

---
+++ src/main/java/com/app/ecommerce/User.java
@@ -0,0 +1,22 @@
+package com.app.ecommerce;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long Id;
+    private String firstName;
+    private String surname;
+}

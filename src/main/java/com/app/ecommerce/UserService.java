ffd9ce9e32e2170f28d86dd589bb7db59369a82b
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-11 13:30:57 -0400

    feat(app): User Service added

---
+++ src/main/java/com/app/ecommerce/UserService.java
@@ -0,0 +1,40 @@
+package com.app.ecommerce;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers(){
+        return userList;
+    }
+
+    public void addUser(User user){
+        user.setId(nextId++);
+        userList.add(user);
+    }
+
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean updateUser(Long id, User userUpdate) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(userUpdate.getFirstName());
+                    existingUser.setSurname(userUpdate.getSurname());
+                    return true;
+                }).orElse(false);
+    }
+}
